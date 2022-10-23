6dbe609f9320d588389caf100851069c67a88210
root <root@abc.ru-central1.internal>
2022-10-23 08:26:08 +0000

    solved the task using C

---
+++ source.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int find_min(int arr[], int n) {
+    int minimal = arr[0];
+    int i;
+    for (i = 1; i < n; i++) {
+        if (arr[i] < minimal) {
+            minimal = arr[i];
+        }
+    }
+    return minimal;
+}
+
+int main(int argc, char** argv) {
+    int arr[10000];
+    int ans[10000];
+    int i, n;
+    scanf("%d", &n);
+    while (n <= 0 || n > 10000) {
+        printf("0 < size of array < 10000");
+        scanf("%d", &n);
+    }
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int minimal = find_min(arr, n);
+    int ans_size = 0;
+    for (i = 0; i < n; i++) {
+        if (arr[i] != minimal) {
+            ans[ans_size] = arr[i];
+            printf("%d", arr[i]);
+            ans_size++;
+        }
+    }
+}
